388f1cba78d62003df742d016296b6863c03dc86
Садыков Марк Раульевич <mrausadykov@sberbank.ru>
2023-02-18 18:09:43 +0300

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import UserProfile from "./components/UserProfile";
 import VKUIWrapper from './wrappers/VKUIWrapper'
+import bridge from '@vkontakte/vk-bridge';
+import {initApp} from './utils/vk';
+
+
 
 const App = () => {
+    bridge.send('VKWebAppInit', {});
+    initApp();
+
   return (
       <VKUIWrapper>
           <UserProfile />
---
+++ src/utils/vk.ts
@@ -0,0 +1,18 @@
+import VKConnect from "@vkontakte/vk-connect";
+
+const APP_ID = 51558383;
+
+export const initApp = () => {
+    const VKConnectCallback = (e) => {
+        if (e.detail.type === 'VKWebAppUpdateConfig') {
+            VKConnect.unsubscribe(VKConnectCallback);
+        }
+    };
+
+    VKConnect.subscribe(VKConnectCallback);
+    return VKConnect.send('VKWebAppInit', {}).then(data => {
+        return data;
+    }).catch(error => {
+        return error;
+    });
+};
