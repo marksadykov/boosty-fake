2a2b9c6b56de842fe78bcd6d7ed06956664c8547
Садыков Марк Раульевич <mrausadykov@sberbank.ru>
2023-02-19 00:31:06 +0300

    setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import bridge from "@vkontakte/vk-bridge";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+bridge.send('VKWebAppInit')
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
