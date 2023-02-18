2ff3731d04c287db0057efe5e28bca8d69bffaf7
Садыков Марк Раульевич <mrausadykov@sberbank.ru>
2023-02-18 15:45:35 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
+const App = () => {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/UserProfile/UserProfile.tsx
@@ -0,0 +1,7 @@
+import s from "./UserProfile.module.scss";
+
+const UserProfile = () => {
+  return <div className={s.test}>UserProfile</div>;
+};
+
+export default UserProfile;
